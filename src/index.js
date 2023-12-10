2f086f66afa443a4f0a6cfe88890c693ed545b48
Lourdilene Souza <lourdilene.souza@gmail.com>
2023-12-10 11:08:42 -0300

    add tests js and dom-api. remove spam in index

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import analyzer from "./analyzer.js";
 const textarea = document.querySelector('[name="user-input"]');
+const button = document.getElementById("reset-button");
 const wordCount = document.querySelector('[data-testid="word-count"]');
 const characterCount = document.querySelector(
   '[data-testid="character-count"]'
@@ -12,7 +13,20 @@ const wordLengthAverage = document.querySelector(
 );
 const numberCount = document.querySelector('[data-testid="number-count"]');
 const numberSum = document.querySelector('[data-testid="number-sum"]');
+
 textarea.addEventListener("keyup", allMetricsUpdate);
+button.addEventListener("click", clearAllMetricsAndText);
+
+function clearAllMetricsAndText() {
+  textarea.value = " ";
+  wordCount.textContent = "Quant. de palavras: 0";
+  characterCount.textContent = "Caracteres: 0";
+  characterNoSpacesCount.textContent = "Quant. caracteres s/ espaços: 0";
+  numberCount.textContent = "Números: 0";
+  numberSum.textContent = "Soma de números: 0";
+  wordLengthAverage.textContent = "Compr. médio das palavras: 0";
+}
+
 function allMetricsUpdate() {
   const text = textarea.value;
   wordCount.textContent = "Quant. de palavras: " + analyzer.getWordCount(text);
@@ -24,5 +38,5 @@ function allMetricsUpdate() {
   numberCount.textContent = "Números: " + analyzer.getNumberCount(text);
   numberSum.textContent = "Soma de números: " + analyzer.getNumberSum(text);
   wordLengthAverage.textContent =
-    "Compr. médio das palavras:" + analyzer.getAverageWordLength(text);
+    "Compr. médio das palavras: " + analyzer.getAverageWordLength(text);
 }
